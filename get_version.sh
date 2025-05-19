ccb3ca188a56d25c52a59be6dad868f369b3227d
marka <>
2025-05-19 14:29:05 +0000

    Put "git-" or "fossil-" in version string to clarify the source of the version number.
    FossilOrigin-Name: 307cefae641c8a8a5ac070f576e550cd158a0cb9198b8c5043f43b19e97fa308

--- get_version.sh
+++ get_version.sh
@@ -4,7 +4,7 @@ srcdir=$1
 [ -n "$srcdir" ]  || exit 1
 echo "#define DATE \"`date -I`\"" >rev.h
 if [ -f $srcdir/.fslckout ] ; then
-  fossil info | sed -n 's/checkout: *\(..........\).*/#define REV "\1"/p' >>rev.h
+  fossil info | sed -n 's/checkout: *\(..........\).*/#define REV "fossil-\1"/p' >>rev.h
 else
-  git rev-parse HEAD  | sed -n 's/^\(.......\).*/#define REV "\1"/p' >>rev.h
+  git rev-parse HEAD  | sed -n 's/^\(.......\).*/#define REV "git-\1"/p' >>rev.h
 fi
